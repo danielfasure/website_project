5473a9007d8ac95f2027b00a4034c4527a60a2e9
danielfasure <danielfasure@gmail.com>
2026-06-19 16:08:24 +0100

    added

--- frontend/testing_string.js
+++ frontend/testing_string.js
@@ -11,3 +11,8 @@ if (tweeting_score > 150) {
 else {
     alert("your tweet " + tweet + " has been tweeted ");
 }
+
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
